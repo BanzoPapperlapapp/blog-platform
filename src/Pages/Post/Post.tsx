862232cc645a4393f5b89e573df42fd8ae2cf617
Mannomann <krivmann00@gmail.com>
2023-02-13 21:53:47 +0200

    добавлен компонент post

--- src/Pages/Post/Post.tsx
+++ src/Pages/Post/Post.tsx
@@ -1,27 +1,45 @@
 import React, {useEffect} from 'react';
+import st from './Post.module.css'
+import def_img from '../../img/blogs/Vector.png'
 import {useAppDispatch, useAppSelector} from "../../app/Store";
 import {getPostTC, setPostAC} from "../../store/PostReducer";
 import { PostApiType} from "../../api/BlogsPlatformApi";
-import {useLocation, useParams} from "react-router-dom";
-import {getPostsTC} from "../../store/PostsReducer";
+import {Link, To, useLocation, useParams} from "react-router-dom";
 import {CircleLoader} from "../../components/Common/UI/Loaders/CircleLoader";
 
 export const Post = () => {
     const {id} = useParams()
-    const post:PostApiType = useLocation().state?.state
+    const localState:PostApiType = useLocation().state?.state
     const dispatch = useAppDispatch()
-    const currPost:PostApiType = useAppSelector(state => state.post)
+    const globalState = useAppSelector(state => state.post)
+
     useEffect(()=>{
-        post
-            ? dispatch(setPostAC(post))
+        localState
+            ? dispatch(setPostAC(localState))
             : id && dispatch(getPostTC(id))
     },[])
 
     return (
-        <div>
-            {currPost.id
-                ? currPost.title
-                : <CircleLoader/>
+        <div className={st.post__container}>
+            <div className={st.post__roadmap}>
+                <Link to={-1 as To} state={{from: 'post'}}>Back to posts</Link>
+            </div>
+            {!globalState.id
+                ? <CircleLoader/>
+                : <>
+                    <div className={st.post__head}>
+                        <div className={st.blog__img}><img src={def_img}/></div>
+                        <h3 className={st.blog__title}>{globalState.blogName}</h3>
+                    </div>
+                    <div className={st.post__body}>
+                        <div className={st.post__title}>
+                            <h3>{globalState.title}</h3>
+                            <span>{globalState.createdAt}</span>
+                        </div>
+                        <div className={st.post__img}><img src={def_img}/></div>
+                        <div className={st.post__text}>{globalState.content}</div>
+                    </div>
+                </>
             }
         </div>
     );
--- src/components/Main/NavigationRoad.tsx
+++ src/components/Main/NavigationRoad.tsx
@@ -16,24 +16,24 @@ export const NavigationRoad = () => {
     let title = 'Page'
 
     const page = useLocation().pathname.split('/')
-    let subTitle:null | string = page[2]
+    let subTitle:null | string = null
+
     if(page[1] === 'blogs') {
         title = 'Blogs'
-        page[2] === blog.id ? subTitle = blog.name : subTitle = page[2]
+        page[2] === blog.id ? subTitle = blog.name : subTitle = ''
     }
 
     if(page[1] === 'posts') {
         title = 'Posts'
-        page[2] === post.id ? subTitle = post.title : subTitle = page[2]
+        page[2] === post.id ? subTitle = post.title : subTitle = ''
 
     }
-    console.log(post.title)
     return (
         <>
             <div>
                 <h3>{title}</h3>
             </div>
-            {page[2] && <div className={st.content__subheader}>
+            {subTitle && <div className={st.content__subheader}>
                 <img src={arr_history}/>
                 {subTitle}
             </div>}
